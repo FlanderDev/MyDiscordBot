a0d3d6a331f1867d660876a2664a189448533e08
FlanDev <55660647+FlanderDev@users.noreply.github.com>
2025-03-19 19:16:20 +0100

    improved application startup

--- DiscordBot/Business/Bots/KumoBot.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace DiscordBot.Business.Bots;
-
-internal sealed class KumoBot(IServiceProvider serviceProvider, char? prefix = null) : BotBase(serviceProvider, prefix)
-{
-}
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -12,16 +12,53 @@ try
 {
     AppDomain.CurrentDomain.UnhandledException += (o, e) => Log.Error(e.ExceptionObject as Exception, "Unhandled Exception.");
 
+    var errorCode = SetRunningDirectory();
+    if (errorCode != null)
+        return errorCode.Value;
 
     Log.Logger = new LoggerConfiguration()
     .WriteTo.File("Log/log.txt", LogEventLevel.Information)
     .WriteTo.Console(LogEventLevel.Verbose) //Default: Verbose
     .CreateLogger();
-
     Log.Information("Initialized logging.");
+
+    var configuration = new ConfigurationBuilder()
+        .AddUserSecrets(Assembly.GetExecutingAssembly(), optional: true)
+        .AddEnvironmentVariables()
+        .Build();
 
     DatabaseContext.CreateDefault();
 
+    await using var serviceProvider = new ServiceCollection()
+        .AddLogging(a => a.SetMinimumLevel(LogLevel.Trace))
+        .AddSingleton<IConfiguration>(configuration)
+        .AddScoped<InaNisBot>()
+        .BuildServiceProvider();
+
+    var testingBot = serviceProvider.GetRequiredService<InaNisBot>();
+    var resultTesting = await testingBot.StartAsync(serviceProvider, configuration["DiscordBot:Token"], configuration["DiscordBot:Name"]);
+
+    Log.Verbose("ready and waiting...");
+    await Task.Delay(Timeout.Infinite);
+    Log.Information("Shutting down...");
+
+    await testingBot.StopAsync();
+
+    Log.Information("Ran to completion.");
+    return 0;
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "Unexpected error. Ending application.");
+    return -1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+static int? SetRunningDirectory()
+{
     var runningSpaceNamespace = typeof(DiscordBot.RunningSpace.RunningSpace).Namespace?.Split('.').LastOrDefault();
     if (string.IsNullOrWhiteSpace(runningSpaceNamespace))
     {
@@ -38,42 +75,5 @@ try
 
     Environment.CurrentDirectory = runningSpace;
     Log.Information($"Running in '{Environment.CurrentDirectory}'.");
-
-
-    var configuration = new ConfigurationBuilder()
-        .AddUserSecrets(Assembly.GetExecutingAssembly(), optional: true)
-        .AddEnvironmentVariables()
-        .Build();
-
-    await using var serviceProvider = new ServiceCollection()
-        .AddLogging(a => a.SetMinimumLevel(LogLevel.Trace))
-        .AddSingleton<IConfiguration>(configuration)
-        .AddScoped<InaNisBot>()
-        .AddScoped<KumoBot>()
-        .BuildServiceProvider();
-
-
-    //var testingBot = serviceProvider.GetRequiredService<TestingBot>();
-    //var testing = await testingBot.StartAsync(serviceProvider);
-
-    var testingBot = serviceProvider.GetRequiredService<InaNisBot>();
-    var resultTesting = await testingBot.StartAsync(serviceProvider, configuration["DiscordBot:Token"], configuration["DiscordBot:Name"]);
-
-    //var kumoBot = serviceProvider.GetRequiredService<KumoBot>();
-    //var resultKumo = await kumoBot.StartAsync(serviceProvider, configuration["DiscordBotTesting:Token"], configuration["DiscordBotTesting:Name"]);
-
-    Log.Verbose("ready and waiting...");
-    await Task.Delay(Timeout.Infinite);
-    Log.Information("Shutting down...");
-
-    await testingBot.StopAsync();
-    //await kumoBot.StopAsync();
-
-    Log.Information("Ran to completion.");
-    return 0;
-}
-catch (Exception ex)
-{
-    Log.Error(ex, "Unexpected error. Ending application.");
-    return -1;
+    return null;
 }
