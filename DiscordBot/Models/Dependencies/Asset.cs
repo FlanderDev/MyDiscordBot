c40f9534a6d8e1f2c26d071107abb3e3a960fc59
FlanDev <55660647+FlanderDev@users.noreply.github.com>
2025-05-13 12:48:45 +0200

    reduced models down to requried data

--- DiscordBot/Models/Dependencies/Asset.cs
+++ DiscordBot/Models/Dependencies/Asset.cs
@@ -4,27 +4,8 @@ namespace DiscordBot.Models.Dependencies;
 
 public sealed class Asset
 {
-    [JsonPropertyName("url")] public string Url { get; set; } = string.Empty;
-
-    [JsonPropertyName("id")] public int? Id { get; set; }
-
-    [JsonPropertyName("node_id")] public string NodeId { get; set; } = string.Empty;
-
-    [JsonPropertyName("name")] public string Name { get; set; } = string.Empty;
-
-    [JsonPropertyName("label")] public object? Label { get; set; }
-
-    [JsonPropertyName("content_type")] public string ContentType { get; set; } = string.Empty;
-
-    [JsonPropertyName("state")] public string State { get; set; } = string.Empty;
-
-    [JsonPropertyName("size")] public int? Size { get; set; }
-
-    [JsonPropertyName("download_count")] public int? DownloadCount { get; set; }
-
-    [JsonPropertyName("created_at")] public DateTime? CreatedAt { get; set; }
-
-    [JsonPropertyName("updated_at")] public DateTime? UpdatedAt { get; set; }
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = string.Empty;
 
     [JsonPropertyName("browser_download_url")]
     public string BrowserDownloadUrl { get; set; } = string.Empty;
--- DiscordBot/Models/Dependencies/Release.cs
+++ DiscordBot/Models/Dependencies/Release.cs
@@ -4,40 +4,6 @@ namespace DiscordBot.Models.Dependencies;
 
 public sealed class Release
 {
-    [JsonPropertyName("url")] public string Url { get; set; } = string.Empty;
-
-    [JsonPropertyName("assets_url")] public string AssetsUrl { get; set; } = string.Empty;
-
-    [JsonPropertyName("upload_url")] public string UploadUrl { get; set; } = string.Empty;
-
-    [JsonPropertyName("html_url")] public string HtmlUrl { get; set; } = string.Empty;
-
-    [JsonPropertyName("id")] public int? Id { get; set; }
-
-    [JsonPropertyName("node_id")] public string NodeId { get; set; } = string.Empty;
-
-    [JsonPropertyName("tag_name")] public string TagName { get; set; } = string.Empty;
-
-    [JsonPropertyName("target_commitish")] public string TargetCommitish { get; set; } = string.Empty;
-
-    [JsonPropertyName("name")] public string Name { get; set; } = string.Empty;
-
-    [JsonPropertyName("draft")] public bool? Draft { get; set; }
-
-    [JsonPropertyName("prerelease")] public bool? Prerelease { get; set; }
-
-    [JsonPropertyName("created_at")] public DateTime? CreatedAt { get; set; }
-
-    [JsonPropertyName("published_at")] public DateTime? PublishedAt { get; set; }
-
-    [JsonPropertyName("assets")] public List<Asset> Assets { get; set; } = [];
-
-    [JsonPropertyName("tarball_url")] public string TarballUrl { get; set; } = string.Empty;
-
-    [JsonPropertyName("zipball_url")] public string ZipballUrl { get; set; } = string.Empty;
-
-    [JsonPropertyName("body")] public string Body { get; set; } = string.Empty;
-
-    [JsonPropertyName("mentions_count")] public int? MentionsCount { get; set; }
+    [JsonPropertyName("assets")]
+    public List<Asset> Assets { get; set; } = [];
 }
-
