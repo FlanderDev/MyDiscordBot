54a2f60baa906f28f6614bca623b6a273f520e75
FlanDev <55660647+FlanderDev@users.noreply.github.com>
2025-05-16 00:52:34 +0200

    adding helper class for blazor specific logic

---
+++ DiscordBot/Business/Helpers/Blazor/Common.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Components;
+using Serilog;
+
+namespace DiscordBot.Business.Helpers.Blazor;
+
+public static class Common
+{
+    public static string? GetRelativeRoute<T>() where T : ComponentBase
+    {
+        var type = typeof(T);
+        var routeAttribute = type.GetCustomAttribute<RouteAttribute>();
+        if (routeAttribute != null)
+            return routeAttribute.Template;
+
+        Log.Error("The component '{component}' does not have a '{route}'.", type.FullName, nameof(RouteAttribute));
+        return null;
+    }
+}
