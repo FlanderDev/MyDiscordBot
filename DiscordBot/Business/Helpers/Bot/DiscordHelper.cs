a4fe9c75940ca1a08e12db9e9441cc3f53cc4e8a
FlanDev <55660647+FlanderDev@users.noreply.github.com>
2025-05-30 13:02:32 +0200

    refactored claims helper added delete to clip manager

--- DiscordBot/Business/Helpers/Bot/DiscordHelper.cs
+++ DiscordBot/Business/Helpers/Bot/DiscordHelper.cs
@@ -8,12 +8,19 @@ internal static class DiscordHelper
 {
     internal static IVoiceChannel? GetVoiceChannel(this ModuleBase<SocketCommandContext> moduleBase) => (moduleBase.Context.User as IGuildUser)?.VoiceChannel;
 
-    internal static ulong? GetUserIdFromHttpContextAccessor(this IHttpContextAccessor httpContextAccessor)
+    internal static (ulong DiscordId, string GlobalName)? GetUserFromHttpContextAccessor(this IHttpContextAccessor httpContextAccessor)
     {
-        var discordClaim = httpContextAccessor.HttpContext?.User.Claims.FirstOrDefault(f => f.Type.Equals(ClaimTypes.Sid));
-        if (discordClaim == null)
+        var sidClaim = httpContextAccessor.HttpContext?.User.Claims.FirstOrDefault(f => f.Type.Equals(ClaimTypes.Sid));
+        if (sidClaim == null)
             return null;
 
-        return ulong.TryParse(discordClaim.Value, out var discordId) ? discordId : null;
+        var discordId = ulong.TryParse(sidClaim.Value, out var idText) ? idText : (ulong?)null;
+        if (discordId == null)
+            return null;
+
+        var nameClaim = httpContextAccessor.HttpContext?.User.Claims.FirstOrDefault(f => f.Type.Equals(ClaimTypes.Name));
+        if (nameClaim == null)
+            return null;
+        return (discordId.Value, nameClaim.Value);
     }
 }
